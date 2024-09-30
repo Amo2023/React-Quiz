9c66b5a16e7fb4e78bbe819e245d77597a44f9cf
Amo C <amochen429@qq.com>
2024-09-30 19:31:25 +1000

    update QuizContext basic structure

---
+++ src/contexts/QuizContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+const QuizContext = createContext();
+
+function QuizProvider({ children }) {
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
+}
+
+function useQuiz() {
+  const context = useContext(QuizContext);
+  if (context === undefined)
+    throw new Error("useQuiz must be used within a QuizProvider");
+  return context;
+}
+
+export { QuizProvider, useQuiz };
